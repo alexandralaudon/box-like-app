ebf7f1abe811bc7f75d2a7a0a2fafffaa2013b85
Alex <alexandralaudon@gmail.com>
2015-04-10 12:47:37 -0600

    Formatting the view

--- app/assets/javascripts/box.js
+++ app/assets/javascripts/box.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
+  $('.hover').hover(function(){
+    $(this).addClass('flip');
+  },function(){
+    $(this).removeClass('flip');
+  });
 
+});
 
-
-
-
-}
-
-One provider number of fields / total number of fields = total percentage of the box
+// One provider number of fields / total number of fields = total percentage of the box
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    @facebook = Facebook.all
+    @google = Google.all
+    @instagram = Instagram.all
   end
 
 end
